d1976c0a777c9d9e1a42823a4402af97a50c0c92
Adhi Setyatuhu <adhi.tuhu@gmail.com>
2024-11-14 21:23:52 +0700

    Add js function to fetch data from open meteo

--- app.js
+++ app.js
@@ -12,3 +12,18 @@ function scrollDailyLeft() {
 
 buttonScollRight.addEventListener('click', scrollDailyRight);
 buttonScrollLeft.addEventListener('click', scrollDailyLeft);
+
+
+let URL = 'https://api.open-meteo.com/v1/forecast?latitude=-6.1818&longitude=106.8223&current=temperature_2m,relative_humidity_2m,precipitation,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min,precipitation_probability_max'
+
+async function getWeatherData() {
+    try {
+        const req = await fetch(URL);
+        const  data = await req.json();
+        console.log(data);
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+getWeatherData();
